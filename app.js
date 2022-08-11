e7157a3a5954c48bd3be9706d9f4beb635d85044
Jill Kidman <jillkidman@gmail.com>
2022-08-11 16:10:22 -0500

    added cart routers into app.js, worked on cart funcionality in cart.js

--- app.js
+++ app.js
@@ -8,6 +8,7 @@ var indexRouter = require("./routes/index");
 var usersRouter = require("./routes/users");
 var authRouter = require("./routes/auth");
 var productsRouter = require("./routes/products");
+var cartRouter = require("./routes/cart");
 
 require("dotenv").config();
 
@@ -36,6 +37,7 @@ app.use("/", indexRouter);
 app.use("/users", usersRouter);
 app.use("/auth", authRouter);
 app.use("/products", productsRouter);
+app.use("/cart", cartRouter);
 
 // catch 404 and forward to error handler
 app.use(function (req, res, next) {
--- routes/cart.js
+++ routes/cart.js
@@ -1,20 +1,58 @@
 var express = require("express");
-const { bakeryDB } = require("../mongo");
 var router = express.Router();
+const { bakeryDB } = require("../mongo");
+const jwt = require('jsonwebtoken');
+const { uuid } = require('uuidv4');
+const dotenv = require('dotenv');
+dotenv.config();
+
 
 /* GET products page. */
-router.get("/cart", async (req, res, next) => {
+router.post("/checkout-cart", async (req, res, next) => {
     try {
-        // need to get current user from DB
-        // const currentUser = 
 
-        // need to get currentCart from current user in DB
-        const collection = await bakeryDB().collection("users.currentCart"); //this will need to be changed to get the current user's cart array
-        const cartProducts = await collection
-            .toArray();
+        /* 
+            1. Get the user token from the headers (req.headers.token)
+            2. Get the userId from the token by using JWT validate
+            3. Get the current user by userId from mongo
+            4. Create a new orderId using uuid
+            5. Create a new order object with the currentCart (from req.body.cart) and the orderId
+            6. Insert the new order into the orders collection
+            7. Update the user to have currentOrderId: orderId
+            8. Save user
+        */
+        const tokenHeaderKey = process.env.TOKEN_HEADER_KEY;
+        const jwtSecretKey = process.env.JWT_SECRET_KEY;
+        const token = req.headers.token;
 
-        res.json({ message: cartProducts });
+        const verifiedToken = jwt.verify(token, jwtSecretKey)
+        const userData = verifiedToken.data
+        const userId = userData.userId
+
+        const userCollection = await bakeryDB().collection("users");
+        const currentUser = await userCollection.findOne({ uid: userId })
+
+        const orderId = uuid()
+        const cart = req.body.cart
+        const newOrder = {
+            orderId,
+            productList: cart
+        }
+
+        const ordersCollection = await bakeryDB().collection("orders");
+        await ordersCollection.insertOne(newOrder)
+
+        userCollection.updateOne({
+            uid: userId
+        }, {
+            $set: {
+                currentOrderId: orderId
+            }
+        })
+
+        res.json({ success: true, message: "Created new order", newOrder, orderId });
     } catch (error) {
+        console.log(error)
         res.status(500).send("error fetching products " + error);
     }
 });
